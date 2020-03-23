0377e2bb5e4ecba11368f3600ddeb2398304d6c6
NTTData <nilofer.perwaiz@nttdata.com>
2020-03-23 09:12:23 +0000

    added some more exception handling

--- src/com/nttdata/nilofer/exceptionhandling/CheckEmailBasicMain.java
+++ src/com/nttdata/nilofer/exceptionhandling/CheckEmailBasicMain.java
@@ -1,28 +1,33 @@
 package com.nttdata.nilofer.exceptionhandling;
 
 import java.util.Scanner;
+//basic checker to see if @ and '.' are there in email
 
-public class CheckEmail {
+public class CheckEmailBasicMain {
 
-	public static void main(String[] args) throws UserIdInvalidException {
+	public static void main(String[] args)  {
 
 		Scanner input = new Scanner(System.in);
 		System.out.println("User Name : ");
 		String userName = input.next();
 		input.close();
-		isEMail(userName)  ;
+		try {
+			isEMail(userName)  ;
+		} catch (UserIdInvalidException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static void isEMail(String userName) throws UserIdInvalidException {
 		boolean isValid;
 		int countAtTheRate = CharacterCount.chkEmail(userName, '@');
 		int countDot = CharacterCount.chkEmail(userName, '.');
-		if ((countAtTheRate == 1) &&((countDot == 1)||(countDot == 2)))
+		if ( (countAtTheRate == 1) && ( (countDot ==1 ) || (countDot == 2) || (countDot ==1 ) ) )
 			isValid = true;
 		else
 			isValid = false;
 		if (!isValid) {
-			throw new UserIdInvalidException("User Id is not an email");
+			throw new UserIdInvalidException("User Id is not a valid email");
 		}
 		else {
 			System.out.println("User Id Valid");
