9bdaac3aa60748f2a3da070ddb16bca92dd62408
NTTData <nilofer.perwaiz@nttdata.com>
2020-03-23 10:28:28 +0000

    updated exception handling for assgn for email validation

---
+++ src/com/nttdata/nilofer/exceptionhandling/ValidateEmail.java
@@ -0,0 +1,49 @@
+package com.nttdata.nilofer.exceptionhandling;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ValidateEmail {
+
+	
+	// Java program to validate email
+	
+		private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+		private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+		public static void emailValidator(String email) throws UserIdInvalidException {
+
+			if (email == null) {
+				System.out.println("Please enter email");
+				email=" ";
+			}
+
+			Matcher matcher = EMAIL_PATTERN.matcher(email);
+			boolean isValid = matcher.matches();
+			// Validate an email address
+						if (!isValid) {
+							throw new UserIdInvalidException("Invalid User Id. PLease enter Valid email");
+							//System.out.println("The email address " + userInput + " is valid");
+						}
+						else {
+							System.out.println("The email address " + email + " is valid");
+						}
+		}
+
+		public static void main(String[] args)
+		{
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter Use Id");
+			String userInput = input.next();
+			try {
+				emailValidator(userInput);
+			} catch (UserIdInvalidException e) {
+				e.printStackTrace();
+			}
+			
+			input.close();
+		}
+	
+}
